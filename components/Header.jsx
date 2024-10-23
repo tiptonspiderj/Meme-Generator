cf4354c8456c334391b09f57a1a18502d92818fc
JEREMY TIPTON <32341894+tiptonspiderj@users.noreply.github.com>
2024-10-23 11:32:05 -0400

    Update Header.jsx

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="header">
             <img 
-                src="/images/trollFace.png" 
+                src="images/trollFace.png" 
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
